b58f370a0f4a1616f81a1b0a180ce0d9318c84d1
Glitch (hello-webpage) <none>
2019-06-26 18:43:00 +0000

    🐩🏖 Checkpoint ./index.html:5235508/115:5235526/868 ./style.css:5235508/82 ./script.js:5235508/210:5235542/86

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-$("#button1").click (function(){
- $("h1").html("Are you REALLY hungry?");
-  $("h2").fadeOut();
-  $("h3").fadeOut();
-  $("#button1").html("No");
-  $("#button2").html("Yes");
-})
-
-
-
-
 
 
 
