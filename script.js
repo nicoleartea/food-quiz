1debf98f07c55ebbcfeda14a1c521dc2ba8d5705
Glitch (hello-webpage) <none>
2019-06-26 15:54:26 +0000

    👝👬 Checkpoint ./script.js:5235508/114:5235542/389 ./index.html:5235508/408

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $("button").click (function(){
-$("h1").html("Are you REALLY hungry?");
-$("button").html()"No")
+ $("h1").html("Are you REALLY hungry?");
+ $("button").html("No");
+ $("button").append("<button id="yesButton">Yes</button>")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
